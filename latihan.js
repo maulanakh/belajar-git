d6bcc276ede32fc4737a6595be09ac7a0f1d4865
Maulana Khairullah <maulana.khairullah12@gmail.com>
2023-11-10 08:50:38 +0700

    membuat projek baru

---
+++ latihan.js
@@ -0,0 +1,39 @@
+// const warna = ["merah","kuning","hijau","biru"];
+
+// // for(let i = 0; i < warna.length; i++){
+// //     console.log(`warna ${warna[i]}`);
+// // }
+// const hari = prompt("Silakan masukkan angka!").toLowerCase;
+
+
+// console.log("Sebelum dibanding");
+
+// if (hari === 'senin'){
+//     console.log(`${hari} adalah monster day`);
+// } else if (hari === 'sabtu'){
+//     console.log("Yey libur")
+// } else{
+//     console.log("cari hari lain")
+// }
+
+// // console.log("Sesuah dibanding");
+// const PASSWORD = 'Pajak123';
+
+// let input = prompt('silakan isi password');
+// while( input !== PASSWORD ){
+//     input = prompt('silakan isi password');
+// }
+// alert('Password Benar');
+
+
+// const warna = ["merah","kuning","hijau","biru"];
+// for ( let color of warna){
+//     console.log (`Ini Warna ${color}`);
+// }
+
+function salam (nama) {
+    console.log (`hai ${nama}. Selamat Siang`);
+}
+
+
+
